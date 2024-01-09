48937945d6ae262d92c6aed64def5c7fa9959453
alansgoncalves <vekio.soft@gmail.com>
2024-01-09 14:36:05 -0300

    consulta maiores faturamentos restaurantes

--- sql/consultas.sql
+++ sql/consultas.sql
@@ -42,3 +42,20 @@ GROUP BY
     r.nome
 ORDER BY renda_total DESC
 
+-- Restaurantes com os maiores faturamentos
+SELECT
+	r.nome,
+	SUM(dp.quantidade * pr.preco_unitario) AS renda_total
+FROM
+    delifoods.detalhes_pedidos dp
+JOIN 
+    delifoods.produtos pr ON dp.id_produto = pr.id_produto
+JOIN 
+    delifoods.pedidos p ON dp.id_pedido = p.id_pedido
+JOIN 
+    delifoods.restaurante r ON pr.id_restaurante = r.id_restaurante
+WHERE p.status = 'Confirmado'
+GROUP BY
+    r.nome
+ORDER BY renda_total DESC
+

a22e4356103e70f14be9c3582015dd439cc10f1f
alansgoncalves <vekio.soft@gmail.com>
2024-01-09 14:35:10 -0300

    consulta prejuízos por cancelamentos

--- sql/consultas.sql
+++ sql/consultas.sql
@@ -25,3 +25,20 @@ GROUP BY
 ORDER BY
     total_pedidos DESC
 
+-- Quanto os restaurantes deixaram de ganhar por conta de cancelamento
+SELECT
+	r.nome,
+	SUM(dp.quantidade * pr.preco_unitario) AS renda_total
+FROM
+    delifoods.detalhes_pedidos dp
+JOIN 
+    delifoods.produtos pr ON dp.id_produto = pr.id_produto
+JOIN 
+    delifoods.pedidos p ON dp.id_pedido = p.id_pedido
+JOIN 
+    delifoods.restaurante r ON pr.id_restaurante = r.id_restaurante
+WHERE p.status = 'Cancelado'
+GROUP BY
+    r.nome
+ORDER BY renda_total DESC
+

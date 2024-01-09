4645611b8a31983d9727cf1a860bf7a7c8b41f3d
alansgoncalves <vekio.soft@gmail.com>
2024-01-09 14:29:44 -0300

    insere dados na tabela restaurante

---
+++ sql/popular_tabelas.sql
@@ -0,0 +1,12 @@
+INSERT INTO delifoods.restaurante(nome,endereco,cidade, telefone, categoria) VALUES
+    ('Sabores Paulistanos', 'Rua da Esperança, 586', 'Diamantina', 98765401, 'Hamburgueria'),
+    ('Sabor da Vila', 'Avenida dos Ipês, 319', 'Diamantina', 45678912, 'Hamburgueria'),
+    ('Cantinho Gastronômico', 'Travessa da Liberdade, 112', 'Diamantina', 10987623, 'Pastelaria'),
+    ('Delícias do Bairro', 'Alameda das Palmeiras, 88', 'Diamantina', 23456781, 'Pizzaria'),
+    ('Gastronomia Urbana', 'Praça do Sol, 108', 'Diamantina', 67890145, 'Pastelaria'),
+    ('Sabor e Arte', 'Viela das Violetas, 764', 'Diamantina', 34567891, 'Pizzaria'),
+    ('Prazeres da Mesa', 'Beco das Maritacas, 913', 'Diamantina', 89012367, 'Restaurante'),
+    ('Rota dos Sabores', 'Rua das Jabuticabeiras, 193', 'Diamantina', 56780123, 'Restaurante'),
+    ('Sabores do Brasil', 'Avenida dos Girassóis, 27', 'Diamantina', 87654321, 'Hamburgueria'),
+    ('Cozinha Paulistana', 'Travessa do Riacho, 951', 'Diamantina', 43210987, 'Restaurante')
+;
